e4191067f65b7446ca8e69977c9cb16f068ddbeb
CamilaRivera <camila.riverart@gmail.com>
2019-08-29 06:24:03 +0000

    Page Downloader refactor and added gitignore

--- fetcher.js
+++ fetcher.js
@@ -1,6 +1,8 @@
 const request = require('request');
 const fs = require('fs');
 
+const input = process.argv;
+
 const readline = require('readline');
 
 const rl = readline.createInterface({
@@ -8,49 +10,52 @@ const rl = readline.createInterface({
   output: process.stdout
 });
 
-const fetcher = (page, localPath) => {
+const fetcher = (url, localPath) => {
   fs.readFile(localPath, (err, data) => {
     if (err) {
       console.log('Error :' + err);
+      rl.close();
       return;
     }
     // If local path is empty
     if (data.length === 0) {
-      req(page, localPath);
+      downloadToPath(url, localPath);
+      rl.close();
     } else {
       rl.question('File is not empty. If you want to over write it type in Y? Else just type enter', (answer) => {
         // If user answer yes
         if (answer === 'Y') {
-          req(page, localPath);
+          downloadToPath(url, localPath);
         } else {
-          console.log("The page wasn't saved");
-          rl.close();
-          return;
+          console.log("The url content wasn't saved");
         }
+        rl.close();
       });
     }
   });
+
+
 };
 
-const writer = function(localPath, body) {
+const writeToFile = function(localPath, body) {
   fs.writeFile(localPath, body, function(err) {
     if (err) {
-      return console.log(err);
+      console.log(err);
     }
-    console.log("Downloaded and saved " + body.length + " bytes to " + localPath);
+    else {
+      console.log("Downloaded and saved " + body.length + " bytes to " + localPath);
+    }
   });
 };
 
-const req = function(page, localPath) {
-  request(page, (error, _, body) => {
+const downloadToPath = function(url, localPath) {
+  request(url, (error, _, body) => {
     if (error) {
       console.log('error:', error); // Print the error if one occurred
-      return;
     } else {
-      writer(localPath, body);
-      rl.close();
+      writeToFile(localPath, body);
     }
   });
 };
 
-fetcher('http://www.example.com', './index.html');
+fetcher(input[2], input[3]);
